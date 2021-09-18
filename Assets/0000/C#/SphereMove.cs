44255161a806b18be079d95d17f27acda4f71661
Underxel <62486145+Underxel@users.noreply.github.com>
2021-09-18 14:44:01 +0300

    Final 7
    koment

---
+++ Assets/0000/C#/SphereMove.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+public class SphereMove : MonoBehaviour
+{ 
+    [Header("Скорость движения")]
+    [SerializeField] private float Speed;
+    private GameObject NAV = null;
+    [Header("Радиус поиска игрока")]
+    [SerializeField] private float Radius;
+
+    private NavMeshAgent Agent;
+    private void Start()
+    {
+        Agent = GetComponent<NavMeshAgent>();
+        Agent.speed = Speed;
+        GetComponent<SphereCollider>().radius = Radius;
+        GetComponent<SphereCollider>().isTrigger = true;
+    }
+
+    void Update()
+    {
+        if (NAV != null)
+        {
+            Agent.SetDestination(NAV.transform.position);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("NAV"))
+        {
+            NAV = other.gameObject;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("NAV"))
+        {
+            NAV = null;
+        }
+    }
+
+}
